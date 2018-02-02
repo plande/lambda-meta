7a1e60d7a34e8249bba2d244e00ae347193a29f9
Panicz Maciej Godek <godek.maciek@gmail.com>
2018-02-03 00:47:01 +0100

    Syntax impansion + some cleanup

--- lambda/meta.scm
+++ lambda/meta.scm
@@ -1,24 +1,7 @@
 (define-module (lambda meta)
   #:use-module (grand scheme)
+  #:use-module (lambda symbol)
   #:use-module (ice-9 regex))
-
-(define new-name-for-symbol
-  (make-parameter (lambda _ (gensym "_"))))
-
-(define ((next-symbol prefix initial) . _)
-  (set! initial (+ initial 1))
-  (pass initial number->string
-	(lambda (s) (string-append prefix s))
-	string->symbol))
-
-(define (new-names args)
-  (match args
-    ((first . rest)
-     `(,((new-name-for-symbol) first) . ,(new-names rest)))
-    (()
-     '())
-    (tail
-     ((new-name-for-symbol) tail))))
 
 (define (extend bindings #;with keys #;mapped-to targets)
   (match `(,keys ,targets)
@@ -35,72 +18,49 @@
  (extend '((a . a*) (b . b*)) #;with '(a . c) '(a** . c**))
  ===> ((c . c**) (a . a**) (a . a*) (b . b*)))
 
-(define ((alpha bindings) expression)
-  (match expression
-    (`(quote ,literal)
-     expression)
+(publish
+ (define alpha-parameter/new-name-for-symbol
+   (make-parameter (lambda _ (gensym "_"))))
 
-    (`(if ,condition ,consequent ,alternative)
-     (let* ((condition* ((alpha bindings) condition))
-	    (consequent* ((alpha bindings) consequent))
-	    (alternative* ((alpha bindings) alternative)))
-       `(if ,condition* ,consequent* ,alternative*)))
+ (define ((alpha bindings) expression)
+   (match expression
+     (`(quote ,literal)
+      expression)
 
-    (`(lambda ,args ,body)
-     (let* ((args* (new-names args))
-	    (bindings* (extend bindings #;with args
-			       #;mapped-to args*))
-	    (body* ((alpha bindings*) body)))
-       `(lambda ,args* ,body*)))
+     (`(if ,condition ,consequent ,alternative)
+      (let* ((condition* ((alpha bindings) condition))
+	     (consequent* ((alpha bindings) consequent))
+	     (alternative* ((alpha bindings) alternative)))
+	`(if ,condition* ,consequent* ,alternative*)))
 
-    (`(,operator . ,operands)
-     `(,((alpha bindings) operator) . ,(map (alpha bindings) operands)))
+     (`(lambda ,args ,body)
+      (let* ((args* (new-names args))
+	     (bindings* (extend bindings #;with args
+				#;mapped-to args*))
+	     (body* ((alpha bindings*) body)))
+	`(lambda ,args* ,body*)))
 
-    (_
-     (cond ((and (symbol? expression)
-		 (assoc expression bindings))
-	    => (lambda ((key . value))
-		 value))
-	   (else
-	    expression)))))
+     (`(,operator . ,operands)
+      `(,((alpha bindings) operator)
+	. ,(map (alpha bindings) operands)))
 
-(define (symbols expression)
-  (match expression
-    (`(,head . ,tail)
-     (union (symbols head) (symbols tail)))
-    (_
-     (if (symbol? expression)
-	 `(,expression)
-	 '()))))
-
-(e.g.
- (symbols '(+ (* 2 3) (/ 4 5))) ===> (* / +))
-
-(define (unbound-symbols expression)
-  (match expression
-    (`(quote ,literal)
-     '())
-    
-    (`(if ,condition ,consequent ,alternative)
-     (union (unbound-symbols condition)
-            (unbound-symbols consequent)
-            (unbound-symbols alternative)))
-
-    (`(lambda ,args ,body)
-     (difference (unbound-symbols body) args))
-
-    (`(,operator . ,operands)
-     (union (unbound-symbols operator)
-            (unbound-symbols operands)))
-
-    (_
-     (if (symbol? expression)
-         `(,expression)
-         '()))))
-
-(e.g.
- (unbound-symbols '(lambda (x) (+ x y))) ===> (y +))
-
+     (_
+      (cond ((and (symbol? expression)
+		  (assoc expression bindings))
+	     => (lambda ((key . value))
+		  value))
+	    (else
+	     expression)))))
+ where
+ (define (new-names args)
+   (match args
+     ((first . rest)
+      `(,((alpha-parameter/new-name-for-symbol) first)
+	. ,(new-names rest)))
+     (()
+      '())
+     (tail
+      ((alpha-parameter/new-name-for-symbol) tail)))))
 
 (define (alpha-normalized expression)
   (let* ((suffixes (filter-map (lambda (symbol)
@@ -110,7 +70,8 @@
 				   (string->number ns)))
 			       (unbound-symbols expression)))
 	 (initial (apply max 0 suffixes)))
-    (parameterize ((new-name-for-symbol (next-symbol "?" initial)))
+    (parameterize ((alpha-parameter-new-name-for-symbol
+		    (next-symbol "?" initial)))
       ((alpha '()) expression))))
 
 (e.g.
@@ -125,13 +86,12 @@
  (alpha-equivalent? '(lambda (x) (+ x x))
 		    '(lambda (y) (+ y y))))
 
-
+(define (idempotent? function value)
+  (equal? (function value)
+	  (function (function value))))
 
 (e.g.
- (let ((idemp-test (lambda (expr) (alpha-equivalent? expr (alpha-normalized expr)))))
-   (and (idemp-test '(lambda (x) (* x x)))
-        (idemp-test '(lambda (x) (* ((lambda (x) (+ x x)) x) ((lambda (x) (* x 3)) x)))))))
-
-    
-
-
+ (and (idempotent? alpha-normalized '(lambda (x) (* x x)))
+      (idempotent? alpha-normalized '(lambda (x)
+				       (* ((lambda (x) (+ x x)) x)
+					  ((lambda (x) (* x 3)) x))))))
